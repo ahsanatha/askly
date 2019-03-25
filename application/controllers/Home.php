004937778e5def1d9f61d0fa2377eb6990aa5745
ahsanatha <ahsan.athallah@gmail.com>
2019-03-25 22:41:49 +0700

    this day closed. i need to sleep

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,6 @@ class Home extends CI_Controller
     public function index()
     {
         $data["title"] = "Askly";
-        $this->load->view('home/index', $data);
+        $this->load->view('Home/index', $data);
     }
 }
--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= $title ?></title>
+    <link rel="shortcut icon" type="image/png" href="assets\imgs\fav.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="assets\css\bootstrap.min.css">
     <link rel="stylesheet" href="assets\css\home.css">
@@ -24,7 +25,6 @@
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto ">
                     <li class="nav-item active mr-3">
@@ -35,51 +35,41 @@
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0 mr-5">
-                    <button class="btn btn-outline-dark my-2 my-sm-0 font-weight-bold" type="submit">AJUKAN
-                        PERTANYAAN</button>
+                    <a href="" class="nav-link">
+                        <button class="btn btn-outline-dark my-2 my-sm-0 font-weight-bold " type="submit" id="ajukan">
+                            AJUKAN PERTANYAAN
+                        </button>
+                    </a>
                 </form>
             </div>
         </nav>
     </div>
     <!-- navbar end-->
     <!-- bagian tengah start -->
-    <div class="d-flex px-5 pt-5" id="tengah">
+    <div class="d-flex pt-5" id="tengah">
         <div class="container">
             <div class="row">
-                <div class="col">
-                    <h1 class=" display-4 font-weight-bolder"> Dari bertanya jadi <br /> mengerti </h1>
-                    <p>Brainly adalah tempat berbagi ilmu 150 juta siswa dan pakar edukasi, belajar bersama
-                        untuk menyelesaikan soal-soal yang paling rumit sekalipun</p>
+                <div class="col-8">
+                    <div class="d-inline-flex flex-column">
+                        <h1 class=" display-4 font-weight-bolder"> Dari bertanya jadi <br /> mengerti </h1>
+                        <p>Brainly adalah tempat berbagi ilmu 150 juta siswa dan pakar edukasi, belajar bersama
+                            untuk menyelesaikan soal-soal yang paling rumit sekalipun</p>
+                    </div>
+                    <div class="d-inline-flex bg-white p-3 " style="width:600px">
+                        <input type="text" style="width:550px" placeholder="Apa pertanyaanmu?" id="pertanyaan">
+                        <button class="ml-1 p-5" id="btn-pertanyaan"><img src="assets\imgs\search.png"
+                                alt="cari"></button>
+                    </div>
                 </div>
-                <div class="col">
-                    <img class="ml-lg-5" src="assets\imgs\hero-animation.svg" alt="" width="400rem" class="ml-2">
+                <div class="col-4">
+                    <img src="assets\imgs\hero-animation.svg" alt="" width="400rem">
                 </div>
             </div>
         </div>
     </div>
     <!-- bagian tengah end -->
     <!-- slide kategori -->
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+
     <!-- end slide kategori -->
 </body>
 
