6357f591d1d920903ead0d698f3c65e720f7facb
ahsanatha <ahsan.athallah@gmail.com>
2019-03-28 23:34:11 +0700

    round up
    trying to make slider with glider js, doesnt seems to work

---
+++ assets/js/home.js
@@ -0,0 +1,10 @@
+new Glider(document.querySelector('.glider'), {
+	slidesToScroll: 1,
+	slidesToShow: 5.5,
+	draggable: true,
+	dots: '.dots',
+	arrows: {
+		prev: '.glider-prev',
+		next: '.glider-next'
+	}
+});
