5e84490fb261836e643ab844facdaff097dd7dde
Febrian <febrian911@gmail.com>
2019-03-28 04:47:55 -0400

    navbar done tipis

--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <title>Login</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= $title ?></title>
@@ -12,7 +13,19 @@
     <script src="assets\js\bootstrap.min.js"></script>
     <script src="assets\js\jquery.min.js"></script>
 </head>
-<nav class="navbar navbar-light bg-dark">
-  <span class="navbar-brand mb-0 h1">Navbar</span>
+<!-- navbar start-->
+<nav class="navbar navbar-light" style="background-color: #3399ff;  box-shadow: 1px 1px 10px grey;">
+    <a class="navbar-brand" href="#">
+        <img src="assets\imgs\800px-Brainly_logo.png" width="90" height="40" alt="askly">
+    </a>
+  <span class="navbar-brand mb-0 h1"></span>
+  <button href="#">nigga</button>
 </nav>
-<img src="assets\imgs\800px-Brainly_logo.png" width="90" height="40" alt="askly">
+<!-- navbar end-->
+<!-- bagian tengah start -->
+<div>
+    <div class="card" style="width: 18rem; height: 30rem;  margin: auto; float: none; margin-bottom: 10px; ">
+        
+    </div>
+</div>
+
