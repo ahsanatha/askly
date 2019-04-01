d763aec360200c2403fee328826be39157481776
Febrian <febrian911@gmail.com>
2019-04-01 15:16:55 -0400

    sentuhan buat register-main done

---
+++ application/views/Register/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $title ?></title>
+    <link rel="shortcut icon" type="image/png" href="assets\imgs\fav.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\css\home.css">
+    <script src="assets\js\bootstrap.min.js"></script>
+    <script src="assets\js\jquery.min.js"></script>
+    <link href="assets\css\sentuhan.css" rel="stylesheet">
+
+</head>
+<!-- navbar start-->
+<nav class="navbar navbar-light" style="background-color: #3399ff;  box-shadow: 1px 1px 10px grey;">
+    <a class="navbar-brand" href="#">
+        <img src="assets\imgs\800px-Brainly_logo.png" width="90" height="40" alt="askly">
+    </a>
+  <span class="navbar-brand mb-0 h1"></span>
+  <button class="btn btn-dark" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
+</nav>
+<!-- navbar end-->
+<!-- bagian tengah start -->
+<body style="background-color: #e6f2ff;">
+<div class="sg-layout__container js-main-container sg-layout__container--reversed-order">
+    <div class="sg-layout__content sg-hide-for-medium-down">
+        <div class="sg-content-box">
+            <div class="sg-content-box__title sg-content-box__title--spaced-top-large">
+                <h1 class="sg-text-bit sg-text-bit--blue-secondary sg-text-bit--large">Selangkah <br> lagi untuk <br> berbagi ilmu</h1>
+            </div>
+        </div>
+    </div>
+    <div class="sg-layout__aside-content">
+        <div class="js-signup-react">
+            <div data-test="authorization-form" class="sg-box sg-box--no-border sg-box--full">
+                <div class="sg-box__hole">
+                    <div class="sg-content-box sg-content-box--full">
+                        <div class="sg-content-box sg-content-box--spaced-bottom-large">
+                            <div class="sg-content-box sg-content-box--spaced-bottom-small">
+                                <h1 class="sg-headline sg-headline--small sg-headline--to-left">Buat akun di Brainly!</h1>
+                            </div>
+                            <div class="sg-text sg-text--small sg-text--gray sg-text--to-left">Agan telah memiliki akun? <span data-test="form-login-link" class="sg-text sg-text--small sg-text--link sg-text--bold sg-text--blue-dark">Masuk di sini</span>
+                            </div>
+                        </div>
+                        <button class="sg-button-primary sg-button-primary--full-width sg-button-primary--fb">
+                            <span class="sg-button-primary__icon">
+                                <div class="sg-icon sg-icon--x16">
+                                    <svg class="sg-icon__svg">
+                                        <use xlink:href="#icon-fb">
+                                            
+                                        </use>
+                                    </svg>
+                                </div>
+                            </span>Dengan Facebook</button>
+                            <div class="sg-content-box sg-content-box--spaced-top sg-content-box--spaced-bottom">
+                                <div class="sg-text sg-text--small sg-text--gray sg-text--bold sg-text--uppercase sg-text--to-center">atau</div></div>
+                                <button class="sg-button-primary sg-button-primary--full-width sg-button-primary--dark">Daftar dengan email</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
