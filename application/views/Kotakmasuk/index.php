f6b4e34a44fd9317fbf42ff0307fe6178479b2cb
Junifar Adam <junifar.18.adam@gmail.com>
2019-04-11 07:29:31 +0700

    WEBPRO_TUBES_KELOMPOK9

---
+++ application/views/Kotakmasuk/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kotak masuk</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $title ?></title>
+    <link rel="shortcut icon" type="image/png" href="assets\imgs\fav.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\css\home.css">
+    <script src="assets\js\bootstrap.min.js"></script>
+    <script src="assets\js\jquery.min.js"></script>
+    <link href="assets\css\sentuhan.css" rel="stylesheet">
+</head>
+<!-- navbar start-->
+<nav class="navbar navbar-light" style="background-color: #3399ff;  box-shadow: 1px 1px 10px grey;">
+    <a class="navbar-brand" href="#">
+        <img src="assets\imgs\800px-Brainly_logo.png" width="90" height="40" alt="askly">
+    </a>
+  <span class="navbar-brand mb-0 h1"></span>
+  <button class="btn btn-ligth" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
+  <button class="btn btn-dark" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
+</nav>
+<!-- navbar end-->
+<!-- bagian field tengah -->
+<body style="background-color: #e6f2ff;">
+    <div class="sg-layout__container js-main-container">
+        <div class="sg-layout__box">
+            <div id="private-messages-container" class="brn-messages js-private-messages brn-messages--open" data-conversations-id="0" data-user-id="0">
+            </div>
+            <section class="brn-messages__conversations js-conversations-panel">
+                <div class="sg-content-box dock-box">
+                    <div class="sg-content-box__header dock-box__top">
+                     <h2 class="sg-headline sg-headline--small">
+                         Pesan
+                     </h2>   
+                    </div>
+                    <div class="sg-content-box__content sg-content-box__content--full">
+                        <ul class="sg-list js-conversations brn-messages__list">
+                        </ul>
+                    </div>
+                    <div class="
+                    sg-content-box__actions">
+                    <div class="sg-text sg-text--small brn-messages__tip">
+                        "Untuk memulai perbincangan, silahkan memilih rekan anda dari"
+                        <svg class="sg-icon--gray-secondary sg-icon--16x">
+                            <use xlink:href=#icon-profile></use>
+                            
+                        </svg>
+                        " Atau kunjungi halaman profilnya"
+                    </div>
+                        
+                    </div>
+                </div>
+            </section>
+            <section class="brn-messages__chatbox brn-chatbox js-chatbox">
+                <div class="brn-loading js-loading">
+                    <div class="brn-loading__hole">
+                        <div class="sg-label">
+                            <div class="sg-label__text">
+                                loading...
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <section data-user-id>
+                    <div>
+                        <h3 class="sg-headline sg-headline--large sg-headline--extra-bold sg-hide-for-small-only">
+                            Tidak ada pesan.
+                        </h3>
+                        <h3 class="sg-headline sg-headline--large sg-hide-for-medium-up">
+                            Tidak ada pesan.
+                        </h3>
+                        <h4 class="sg-text-block sg-text-block--gray">
+                            "Kunjungi profil  temanmu dan undang mereka untuk berkirim pesan"
+                        </h4>
+                    </div>
+                </section>
+            </section>
+                            
+                        </div>
+                        
+                    
+                </div>
+                
+            
+</body>
+</html>
+
--- application/views/Login/index.php
+++ application/views/Login/index.php
@@ -19,43 +19,41 @@
     <a class="navbar-brand" href="#">
         <img src="assets\imgs\800px-Brainly_logo.png" width="90" height="40" alt="askly">
     </a>
-    <span class="navbar-brand mb-0 h1"></span>
-    <button class="btn btn-dark" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
+  <span class="navbar-brand mb-0 h1"></span>
+  <button class="btn btn-dark" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
 </nav>
 <!-- navbar end-->
 <!-- bagian tengah start -->
-
 <body style="background-color: #e6f2ff;">
-    <div class="sg-layout__container js-main-container sg-layout__container--reversed-order">
-        <div class="sg-layout__content sg-hide-for-medium-down">
-            <div class="sg-content-box">
-                <div class="sg-content-box__title sg-content-box__title--spaced-top-large">
-                    <h1 class="sg-text-bit sg-text-bit--blue-secondary sg-text-bit--large">Hari ini <br> update <br>
-                        ilmu apa?</h1>
-                </div>
+<div class="sg-layout__container js-main-container sg-layout__container--reversed-order">
+    <div class="sg-layout__content sg-hide-for-medium-down">
+        <div class="sg-content-box">
+            <div class="sg-content-box__title sg-content-box__title--spaced-top-large">
+                <h1 class="sg-text-bit sg-text-bit--blue-secondary sg-text-bit--large">Hari ini <br> update <br> ilmu apa?</h1>
             </div>
         </div>
-        <div class="sg-layout__aside-content">
-            <div class="sg-layout__box">
-                <div class="sg-content-box js-sign-in-react">
-                    <div data-test="authentication-form" class="sg-content-box">
-                        <div class="sg-content-box sg-content-box--spaced-bottom-large">
-                            <div class="sg-content-box sg-content-box--spaced-bottom-small">
-                                <h1 class="sg-headline sg-headline--small sg-headline--to-left">Masuk</h1>
-                            </div>
+    </div>
+    <div class="sg-layout__aside-content">
+        <div class="sg-layout__box">
+            <div class="sg-content-box js-sign-in-react">
+                <div data-test="authentication-form" class="sg-content-box">
+                    <div class="sg-content-box sg-content-box--spaced-bottom-large">
+                        <div class="sg-content-box sg-content-box--spaced-bottom-small">
+                            <h1 class="sg-headline sg-headline--small sg-headline--to-left">Masuk</h1>
                         </div>
-                        <div class="sg-content-box sg-content-box--spaced-bottom-large">
-                            <div class="sg-content-box__content sg-content-box__content--with-centered-text">
-                                <button class="sg-button-primary sg-button-primary--full-width sg-button-primary--fb">
-                                    <span class="sg-button-primary__icon">
-                                        <div class="sg-icon sg-icon--x16">
-                                            <svg class="sg-icon__svg">
-                                                <use xlink:href="#icon-fb">
-
-                                                </use>
-                                            </svg>
-                                        </div>
-                                    </span>Dengan Facebook</button>
+                    </div>
+                    <div class="sg-content-box sg-content-box--spaced-bottom-large">
+                        <div class="sg-content-box__content sg-content-box__content--with-centered-text">
+                            <button class="sg-button-primary sg-button-primary--full-width sg-button-primary--fb">
+                                <span class="sg-button-primary__icon">
+                                    <div class="sg-icon sg-icon--x16">
+                                        <svg class="sg-icon__svg">
+                                            <use xlink:href="#icon-fb">
+                                                
+                                            </use>
+                                        </svg>
+                                    </div>
+                                </span>Dengan Facebook</button>
                             </div>
                         </div>
                         <div class="sg-content-box sg-content-box--spaced-bottom">
@@ -63,7 +61,7 @@
                                 <div class="sg-actions-list">
                                     <div class="sg-actions-list__hole sg-actions-list__hole--grow">
                                         <div class="sg-horizontal-separator">
-
+                                            
                                         </div>
                                     </div>
                                     <div class="sg-actions-list__hole">
@@ -71,7 +69,7 @@
                                     </div>
                                     <div class="sg-actions-list__hole sg-actions-list__hole--grow">
                                         <div class="sg-horizontal-separator">
-
+                                            
                                         </div>
                                     </div>
                                 </div>
@@ -79,66 +77,49 @@
                         </div>
                         <form>
                             <div class="sg-content-box__content sg-content-box__content--spaced-bottom">
-                                <input type="text" class="sg-input sg-input--full-width"
-                                    placeholder="Nama panggilan atau email " name="username" value="">
+                                <input type="text" class="sg-input sg-input--full-width" placeholder="Nama panggilan atau email " name="username" value="">
                             </div>
                             <div class="sg-content-box__content sg-content-box__content--spaced-bottom">
-                                <input type="password" class="sg-input sg-input--full-width" placeholder="Kata sandi"
-                                    name="password" value="">
+                                <input type="password" class="sg-input sg-input--full-width" placeholder="Kata sandi" name="password" value="">
                             </div>
                             <div class="sg-content-box__content sg-content-box__content--spaced-bottom-large">
                                 <div class="sg-actions-list">
                                     <div class="sg-actions-list__hole">
                                         <div class="sg-actions-list sg-actions-list--no-wrap">
-                                            <div
-                                                class="sg-actions-list__hole sg-actions-list__hole--container sg-actions-list__hole--no-shrink">
-                                                <div class="sg-checkbox">
-                                                    <input class="sg-checkbox__element" type="checkbox" id="autoLogin"
-                                                        name="autoLogin" checked=""><label class="sg-checkbox__ghost"
-                                                        for="autoLogin">
-                                                        <div class="sg-icon sg-icon--adaptive sg-icon--x10">
-                                                            <svg class="sg-icon__svg">
-                                                                <use xlink:href="#icon-check">
-
-                                                                </use>
-                                                            </svg>
-                                                        </div>
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div class="sg-actions-list__hole">
-                                                <label for="autoLogin"
-                                                    class="sg-text sg-text--xsmall sg-text--gray sg-text--bold sg-text--justify">Saya
-                                                    ingin tetap masuk</label>
+                                            <div class="sg-actions-list__hole sg-actions-list__hole--container sg-actions-list__hole--no-shrink"><div class="sg-checkbox">
+                                                <input class="sg-checkbox__element" type="checkbox" id="autoLogin" name="autoLogin" checked=""><label class="sg-checkbox__ghost" for="autoLogin">
+                                                    <div class="sg-icon sg-icon--adaptive sg-icon--x10">
+                                                        <svg class="sg-icon__svg">
+                                                            <use xlink:href="#icon-check">
+                                                                
+                                                            </use>
+                                                        </svg>
+                                                    </div>
+                                                </label>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="sg-actions-list__hole sg-actions-list__hole--to-right">
-                                        <a href="#"
-                                            class="sg-text sg-text--xsmall sg-text--link sg-text--bold sg-text--blue-dark">Lupa
-                                            kata sandi?</a>
+                                        <div class="sg-actions-list__hole">
+                                            <label for="autoLogin" class="sg-text sg-text--xsmall sg-text--gray sg-text--bold sg-text--justify">Saya ingin tetap masuk</label>
+                                        </div>
                                     </div>
                                 </div>
+                                <div class="sg-actions-list__hole sg-actions-list__hole--to-right">
+                                    <a href="#" class="sg-text sg-text--xsmall sg-text--link sg-text--bold sg-text--blue-dark">Lupa kata sandi?</a>
+                                </div>
                             </div>
-                            <div class="sg-content-box__content sg-content-box__content--spaced-bottom-xlarge">
-                                <button data-test="form-login-submit"
-                                    class="sg-button-primary sg-button-primary--alt">Masuk</button>
-                            </div>
-                        </form>
-                        <div class="sg-content-box__content">
-                            <div class="sg-text sg-text--xsmall sg-text--gray sg-text--bold">Saya tidak memiliki akun,
-                                <a href="#"
-                                    class="sg-text sg-text--xsmall sg-text--link sg-text--bold sg-text--blue-dark">mendaftar
-                                    sekarang</a>. </div>
                         </div>
+                        <div class="sg-content-box__content sg-content-box__content--spaced-bottom-xlarge">
+                            <button data-test="form-login-submit" class="sg-button-primary sg-button-primary--alt">Masuk</button>
+                        </div>
+                    </form>
+                    <div class="sg-content-box__content">
+                        <div class="sg-text sg-text--xsmall sg-text--gray sg-text--bold">Saya tidak memiliki akun, <a href="#" class="sg-text sg-text--xsmall sg-text--link sg-text--bold sg-text--blue-dark">mendaftar sekarang</a>. </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 </body>
 
-</div>
-</div>
-</body>
