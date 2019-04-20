43c387aada3bb1a0a8b3d954d40d049265d4f2b7
ahsanatha <ahsan.athallah@gmail.com>
2019-04-21 02:13:30 +0700

    make profile

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -1,5 +1,26 @@
 <body>
-    <div class="container">
+    <div class="container justify-content-center">
+        <div class="col-11">
+            <div class="container mt-5">
+                <div class="card">
+                    <div class="card-header text-right">
+                        2.122 poin
+                    </div>
+                    <div class="card-body">
+                        <img src="assets\imgs\avatar\user.svg" alt="User" width="90">
+                        <div class="ml-3 d-inline-block">
+                            <h5 class="card-title ">Ahsan Athallah</h5>
+                            <h5 class="card-title ">Pakar</h5>
+                        </div>
 
+                        <p class="card-text ">Nama depan : </p>
+                        <p class="card-text ">Tingkat Pendidikan : </p>
+                        <p class="card-text ">Usia : </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+
 </body>
