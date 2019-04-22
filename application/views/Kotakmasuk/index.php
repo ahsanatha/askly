114d3023778b7a96ef45c3cd2ae5d0dcfeaf0a44
Febrian Kurniawan <febrian911@gmail.com>
2019-04-22 13:25:45 +0700

    timeline lagi

--- application/views/Kotakmasuk/index.php
+++ application/views/Kotakmasuk/index.php
@@ -13,79 +13,127 @@
     <script src="assets\js\bootstrap.min.js"></script>
     <script src="assets\js\jquery.min.js"></script>
     <link href="assets\css\sentuhan.css" rel="stylesheet">
+    <script src="https://styleguide.brainly.com/images/icons-d4009d7e19.js" async=""></script>
+    <script src="https://styleguide.brainly.com/images/subjects-icons-0676dae371.js" async=""></script>
 </head>
 <!-- navbar start-->
-<nav class="navbar navbar-light" style="background-color: #3399ff;  box-shadow: 1px 1px 10px grey;">
-    <a class="navbar-brand" href="#">
-        <img src="assets\imgs\800px-Brainly_logo.png" width="90" height="40" alt="askly">
-    </a>
-  <span class="navbar-brand mb-0 h1"></span>
-  <button class="btn btn-ligth" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
-  <button class="btn btn-dark" style="border-radius: 10px; box-shadow: 1px 1px 10px grey" href="#">nigga</button>
-</nav>
+<div class="sg-header__container">
+    <div class="sg-header__content">
+        <div class="sg-header__left">
+            <a href="/" class="sg-home-button" title="Brainly.com - For students. By students.">
+                <img class="sg-home-button__logo-small" src="https://styleguide.brainly.com/images/logos/brainly-mobile-426ef8718f.svg" alt="Brainly.com">
+                <img class="sg-home-button__logo-big" src="https://styleguide.brainly.com/images/logos/brainly-761d75d6ea.svg" alt="Brainly.com">
+            </a>
+        </div>
+        <div class="sg-header__middle">
+            <div class="sg-content-box sg-content-box--full js-react-search-bar">
+                <form class="sg-search" method="GET" action="/app/ask" role="search">
+                    <input type="hidden" name="entry" value="top">
+                    <input required="" class="sg-input sg-input--with-icon sg-search__input sg-input--full-width sg-input--light js-top-search-input" name="q" type="search" placeholder="What is your question?" data-hj-whitelist="" aria-label="Search">
+                    <div class="sg-search__icon">
+                        <svg class="sg-icon sg-icon--x18  sg-icon--adaptive">
+                            <title>Search</title>
+                            <use xlink:href="#icon-search">
+                                
+                            </use>
+                        </svg>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="sg-header__right">
+            <div class="sg-actions-list">
+                </div></div></div></div>
 <!-- navbar end-->
 <!-- bagian field tengah -->
 <body style="background-color: #e6f2ff;">
     <div class="sg-layout__container js-main-container">
-        <div class="sg-layout__box">
-            <div id="private-messages-container" class="brn-messages js-private-messages brn-messages--open" data-conversations-id="0" data-user-id="0">
+        <div class="sg-layout__content sg-layout__content--no-max-width">
+            <div class="js-message-box">
+                <div class="sg-content-box sg-content-box--spaced-bottom-large sg-hide-for-small-only"></div>
+                <div class="sg-content-box sg-hide-for-medium-up">
+                    <div class="sg-box sg-box--blue-secondary sg-box--full sg-box--no-border-radius sg-box--no-border">
+                        <div class="sg-box__close js-message-box-close">
+                            <svg class="sg-icon sg-icon--dark sg-icon--x10">
+                                <use xlink:href="#icon-x">
+                                    
+                                </use>
+                            </svg>
+                        </div>
+                        <div class="sg-box__hole">
+                            <div class="sg-actions-list sg-actions-list--to-top sg-actions-list--no-wrap"><div class="sg-actions-list__hole">
+                                <div class="sg-avatar sg-avatar--spaced">
+                                    <img class="sg-avatar__image" src="//brainly.com/app/../app/img/icons/icon_information_round.svg"></div>
+                                </div>
+                                <div class="sg-actions-list__hole sg-actions-list__hole--grow">
+                                    <div class="sg-content-box">
+                                        <div class="sg-content-box__content sg-content-box__content--spaced-bottom-xsmall sg-content-box__content--spaced-top-small">
+                                            <div class="sg-text sg-text--small sg-text--bold">
+                                                Brainly will be getting rid of private messages and profile comments.
+                                            </div>
+                                        </div>
+                        <div class="sg-content-box__content sg-content-box__content--spaced-bottom-small">
+                            <div class="sg-text sg-text--small">
+                        Brainly will be getting rid of private messages and profile comments.
+                        <a href="https://faq.brainly.com/hc/en-us/articles/360001227690-Brainly-will-be-getting-rid-of-private-messages-and-profile-comments-" target="_blank" class="sg-text sg-text--bold sg-text--small sg-text--gray sg-text--link">Please read out blog post for more details.</a>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <section class="brn-messages__conversations js-conversations-panel">
-                <div class="sg-content-box dock-box">
-                    <div class="sg-content-box__header dock-box__top">
-                     <h2 class="sg-headline sg-headline--small">
-                         Pesan
-                     </h2>   
+        </div>
+    </div>
+</div>
+<div class="sg-layout__box">
+    <div id="private-messages-container" class="brn-messages js-private-messages brn-messages--open" data-conversation-id="0" data-user-id="0">
+        <section style="box-sizing: box-border;"class="brn-messages__conversations js-conversations-panel">
+            <div class="sg-content-box dock-box">
+                <div class="sg-content-box__header dock-box__top"><h2 class="sg-headline sg-headline--small">
+                        Messages
+                        </h2>
                     </div>
-                    <div class="sg-content-box__content sg-content-box__content--full">
-                        <ul class="sg-list js-conversations brn-messages__list">
-                        </ul>
-                    </div>
-                    <div class="
-                    sg-content-box__actions">
-                    <div class="sg-text sg-text--small brn-messages__tip">
-                        "Untuk memulai perbincangan, silahkan memilih rekan anda dari"
-                        <svg class="sg-icon--gray-secondary sg-icon--16x">
-                            <use xlink:href=#icon-profile></use>
-                            
-                        </svg>
-                        " Atau kunjungi halaman profilnya"
-                    </div>
+                    <div class="sg-content-box__content sg-content-box__content--full"><ul class="sg-list js-conversations brn-messages__list">
                         
+                    </ul>
+                </div>
+                <div class="sg-content-box__actions">
+                    <div class="sg-text sg-text--small brn-messages__tip">
+                        To start a conversation, choose a user from <svg class="sg-icon sg-icon--gray-secondary sg-icon--x16">
+                            <use xlink:href="#icon-profile">
+                                
+                            </use>
+                        </svg> or go to the user's profile.
+                        </div>
                     </div>
                 </div>
             </section>
             <section class="brn-messages__chatbox brn-chatbox js-chatbox">
-                <div class="brn-loading js-loading">
-                    <div class="brn-loading__hole">
-                        <div class="sg-label">
-                            <div class="sg-label__text">
-                                loading...
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <section data-user-id>
-                    <div>
-                        <h3 class="sg-headline sg-headline--large sg-headline--extra-bold sg-hide-for-small-only">
-                            Tidak ada pesan.
-                        </h3>
-                        <h3 class="sg-headline sg-headline--large sg-hide-for-medium-up">
-                            Tidak ada pesan.
-                        </h3>
-                        <h4 class="sg-text-block sg-text-block--gray">
-                            "Kunjungi profil  temanmu dan undang mereka untuk berkirim pesan"
-                        </h4>
-                    </div>
-                </section>
-            </section>
-                            
-                        </div>
-                        
-                    
-                </div>
-                
-            
-</body>
-</html>
+                                
+
+                            <section data-user-id="">
+
+                                <div>
+
+                                    
+                                        <h3 class="sg-headline sg-headline--large sg-headline--extra-bold sg-hide-for-small-only">
+                                            No messages.
+                                        </h3>
+                                        <h3 class="sg-headline sg-headline--large sg-hide-for-medium-up">
+                                            No messages.
+                                        </h3>
+
+                                    
+                                    
+                                        <h4 class="sg-text-block sg-text-block--gray">
+                                            Go to someone's profile to invite them to a conversation.
+                                        </h4>
+
+                                    
+                                                
+                                </div>
+                            </section>
+                        </section></div></div></div></div>
+                        </body>
+                        </html>
 
